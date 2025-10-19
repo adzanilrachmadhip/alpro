640974717db4bb77ee7e9584d6067e5f97f0143f
Adzanil Rachmadhi <adzanilrachmadhiputra@gmail.com>
2025-10-19 22:43:55 +0700

    add code to mater nested loop

---
+++ materi4-nested-loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class nested_looping
+{
+
+    public static void Main()
+    {
+        Console.WriteLine("Inputkan perulangan Outer yang anda inginkan ?? = ");
+        int outerLimit = Convert.ToInt32(Console.ReadLine());
+        int outerNumber = 1;
+        Console.WriteLine("Inputkan perulangan Outer yang anda inginkan ?? = ");
+        int innerLimit = Convert.ToInt32(Console.ReadLine());
+        int x = 1;
+        do{
+            Console.WriteLine($"{outerNumber}. Outer Loop Yang ke {0} ", x);
+            int y = 1;
+            do{
+                Console.WriteLine("   => Inner Loop yang ke {0} ", y);
+                y++;
+            } while (y <= innerLimit);
+        x++;
+        outerNumber++;
+        }while (x <= outerLimit);
+    }
+}    
